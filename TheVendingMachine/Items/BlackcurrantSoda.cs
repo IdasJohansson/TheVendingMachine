4706184475a4523437456946fd74e0d35985c422
ida <idasusannajohansson@gmail.com>
2022-12-01 18:23:21 +0100

    Upd

---
+++ TheVendingMachine/Items/BlackcurrantSoda.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TheVendingMachine.Items
+{
+	public class BlackcurrantSoda
+	{
+		public BlackcurrantSoda()
+		{
+		}
+	}
+}
+
--- TheVendingMachine/Items/LemonSorbet.cs
+++ TheVendingMachine/Items/LemonSorbet.cs
@@ -10,12 +10,10 @@ namespace TheVendingMachine.Items
         public void AddProductInfo()
         {
             // Creating a Sorbet objekt
-            Product product = null;
-            product = Product.CreateProduct("Sorbet");
+            Product product = Product.CreateProduct("Sorbet");
 
             // Getting a Sorbet objekt of the type LemonSorbet
-            IProduct iproduct = null;
-            iproduct = product.GetProduct("LemonSorbet");
+            IProduct iproduct = product.GetProduct("LemonSorbet");
 
             // Giving the LemonSorbet objekt values
             product.ProductId = 3;
@@ -23,57 +21,55 @@ namespace TheVendingMachine.Items
             product.ProductInfo = "250ml";
             product.ProductCost = 25;
 
-            // Objekt to List of products 
+            // Add Objekt to List of products 
             Product.products.Add(product);
         }
 
         public void Description()
         {
-            Console.WriteLine("This is cold sorbet");
+            Console.WriteLine();
+            DateTime date = DateTime.Now;
+            DateTime bestBefore = date.AddMonths(1);
+            Console.WriteLine($"This sorbet is made of sugar and fruitjuice extracted from Lemons harvested in the southern of Sweden. Best-before date: {bestBefore.ToString("dd-MM-yyyy")} "); 
         }
 
         public void Buy()
         {
-            // Vid köp av produkt så skall en kontroll ske; att användaren har
-            // matat in tillräckligt med pengar till automaten.Om inte så
-            // stoppas köpet.
-
-            Wallet.ViewBalance();
-
-            if (true)
+            try
             {
-                Console.WriteLine("Lets buy this product");
                 // Användaren ska kunna acceptera köpet eller välja att gå tillbaka till menyn
+                Console.WriteLine();
                 Console.WriteLine("Are you sure you want to buy this product? ");
                 Console.WriteLine("Press 1 to confirm and 0 to return to menu.");
+                Console.WriteLine();
+
                 int confirm = Convert.ToInt32(Console.ReadLine());
                 if (confirm == 1)
                 {
                     // Vid köpa av produkt ska produkten köpas och användas
-                    Use();
-                    Console.WriteLine("Press a key to return to menu");
-                    Console.ReadKey();
-
                     // Användaren tas tillbaka till menyn vid avslutat köp:
-                    Menus.StartMenu();
+                    Use();
                 }
                 else
                 {
                     // Användaren ska kunna acceptera köpet eller välja att gå tillbaka till menyn
                     Menus.StartMenu();
                 }
-            }
-            else
-            {
-                Console.WriteLine("Insert more money in machine");
-            }
 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Helper.ReturnMenuMessage();
+            }
         }
 
         public void Use()
         {
-            Console.WriteLine("*Eating this good sorbet*");
-     
+            Console.Clear();
+            Console.WriteLine("Purchase confirmed");
+            Console.WriteLine("Num num num...Cold and refreshing!");
+            Helper.ReturnMenuMessage();
         }
     }
 }
