024c906a8d2407ce1db2c4cd4b93fb26ecfffedc
ida <idasusannajohansson@gmail.com>
2022-12-09 14:06:18 +0100

    Added try catch and removed uneccesary code

--- TheVendingMachine/Product/Product.cs
+++ TheVendingMachine/Product/Product.cs
@@ -41,66 +41,92 @@ namespace TheVendingMachine.Items
         // Metod som returnerar en ny instans av en produktkategori, anropas när man ska skapa en produkt och tilldela den värden. 
         public static Product CreateProduct(string ProductCategory)
         {
-            if (ProductCategory.Equals("Soda"))
-                return new Soda();
-            else if (ProductCategory.Equals("Sorbet"))
-                return new Sorbet();
-            else if (ProductCategory.Equals("Berry"))
-                return new Berry();
-            else
-                Helper.ErrorColor("Something went wrong when creating product category.");
-            Helper.ReturnMenuMessage();
-            return null;
+            try
+            {
+                if (ProductCategory.Equals("Soda"))
+                    return new Soda();
+                else if (ProductCategory.Equals("Sorbet"))
+                    return new Sorbet();
+                else if (ProductCategory.Equals("Berry"))
+                    return new Berry();
+                else
+                    Helper.ErrorColor("Something went wrong when creating product category.");
+                    Helper.ReturnMenuMessage();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Helper.ReturnMenuMessage();
+                return null;
+            }
         }
 
         // Metod som returnerar en instans av respektove produkt
         public IProduct GetProduct(string ProductItemType)
         {
-            if (ProductItemType.Equals("AppleSoda"))
-                return new AppleSoda();
-            else if (ProductItemType.Equals("BlackcurrantSoda"))
-                return new BlackcurrantSoda();
-            else if (ProductItemType.Equals("CherrySoda"))
-                return new CherrySoda();
-            else if (ProductItemType.Equals("Raspberry"))
-                return new Raspberry();
-            else if (ProductItemType.Equals("Strawberry"))
-                return new Strawberry();
-            else if (ProductItemType.Equals("Wildberry"))
-                return new Wildberry();
-            else if (ProductItemType.Equals("LemonSorbet"))
-                return new LemonSorbet();
-            else if (ProductItemType.Equals("ElderberrySorbet"))
-                return new ElderberrySorbet();
-            else if (ProductItemType.Equals("MangoSorbet"))
-                return new MangoSorbet();
-            else
-                Helper.ErrorColor("Something went wrong when creating products.");
+            try
+            {
+                if (ProductItemType.Equals("AppleSoda"))
+                    return new AppleSoda();
+                else if (ProductItemType.Equals("BlackcurrantSoda"))
+                    return new BlackcurrantSoda();
+                else if (ProductItemType.Equals("CherrySoda"))
+                    return new CherrySoda();
+                else if (ProductItemType.Equals("Raspberry"))
+                    return new Raspberry();
+                else if (ProductItemType.Equals("Strawberry"))
+                    return new Strawberry();
+                else if (ProductItemType.Equals("Wildberry"))
+                    return new Wildberry();
+                else if (ProductItemType.Equals("LemonSorbet"))
+                    return new LemonSorbet();
+                else if (ProductItemType.Equals("ElderberrySorbet"))
+                    return new ElderberrySorbet();
+                else if (ProductItemType.Equals("MangoSorbet"))
+                    return new MangoSorbet();
+                else
+                    Helper.ErrorColor("Something went wrong when creating products.");
+                    Helper.ReturnMenuMessage();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
                 Helper.ReturnMenuMessage();
-            return null;
+                return null;
+            }
         }
 
         // Adderar info till alla produkter som ska säljas i maskinen
         public static void AddProductInfoWrapper()
         {
-            AppleSoda appleSoda = new AppleSoda();
-            appleSoda.AddProductInfo();
-            BlackcurrantSoda blackcurrantSoda = new BlackcurrantSoda();
-            blackcurrantSoda.AddProductInfo();
-            CherrySoda cherrySoda = new CherrySoda();
-            cherrySoda.AddProductInfo();
-            ElderberrySorbet elderberrySorbet = new ElderberrySorbet();
-            elderberrySorbet.AddProductInfo();
-            LemonSorbet lemonSorbet = new LemonSorbet();
-            lemonSorbet.AddProductInfo();
-            MangoSorbet mangoSorbet = new MangoSorbet();
-            mangoSorbet.AddProductInfo();
-            Raspberry raspberry = new Raspberry();
-            raspberry.AddProductInfo();
-            Strawberry strawberry = new Strawberry();
-            strawberry.AddProductInfo();
-            Wildberry wildberry = new Wildberry();
-            wildberry.AddProductInfo();
+            try
+            {
+                AppleSoda appleSoda = new AppleSoda();
+                appleSoda.AddProductInfo();
+                BlackcurrantSoda blackcurrantSoda = new BlackcurrantSoda();
+                blackcurrantSoda.AddProductInfo();
+                CherrySoda cherrySoda = new CherrySoda();
+                cherrySoda.AddProductInfo();
+                ElderberrySorbet elderberrySorbet = new ElderberrySorbet();
+                elderberrySorbet.AddProductInfo();
+                LemonSorbet lemonSorbet = new LemonSorbet();
+                lemonSorbet.AddProductInfo();
+                MangoSorbet mangoSorbet = new MangoSorbet();
+                mangoSorbet.AddProductInfo();
+                Raspberry raspberry = new Raspberry();
+                raspberry.AddProductInfo();
+                Strawberry strawberry = new Strawberry();
+                strawberry.AddProductInfo();
+                Wildberry wildberry = new Wildberry();
+                wildberry.AddProductInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Helper.ReturnMenuMessage();
+            }
         }
 
     }
--- TheVendingMachine/Program.cs
+++ TheVendingMachine/Program.cs
@@ -2,31 +2,6 @@
 using TheVendingMachine.Items;
 using TheVendingMachine.Services;
 
-
-//// Create the Sodafactory object by passing the factory type as Soda
-//// Abstract factory
-//Product product = null;
-//product = Product.CreateProduct("Soda");
-//Console.WriteLine("Product type: " + product.GetType().Name);
-
-//// Get AppleSoda object by passing the soda type as AppleSoda
-//// interface 
-//IProduct iproduct = null;
-//iproduct = product.GetProduct("AppleSoda");
-//Console.WriteLine("Soda type: " + iproduct.GetType().Name);
-//var apple = iproduct.GetType().Name;
-
-//// Method
-//string description = null;
-//// Reach the Applesodas methods if its with returntype string
-//description = iproduct.Description();
-//Console.WriteLine(apple + " " + description);
-
-//product.ProductId = 1;
-//product.ProductName = "AppleSoda";
-//product.ProductInfo = "330ml"; 
-//product.ProductCost = 15;
-
 // Anropar metoden som ropar på AddProductInfo till varje produkt
 Product.AddProductInfoWrapper(); 
 
